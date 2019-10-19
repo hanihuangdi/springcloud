58a42e16b0f07ea040921ade68422542661e7858
xiaofu <759983718@qq.com>
2019-10-19 17:43:58 +0800

    configServer

---
+++ configClient/src/main/java/southwind/controller/ConfigclientHandler.java
@@ -0,0 +1,17 @@
+package southwind.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/configclient")
+public class ConfigclientHandler {
+    @Value("${server.port}")
+    private String port;
+    @GetMapping("/find")
+    public String find(){
+        return  port;
+    }
+}
